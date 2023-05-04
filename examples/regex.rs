3519dce7175f30678708e15b03d372cda53a4baa
Stanley Zhu <syzhu00@gmail.com>
2023-05-03 23:09:59 -0500

    Match the string, not sure if this is actually a circuit..

--- examples/regex.rs
+++ examples/regex.rs
@@ -4,11 +4,13 @@ use halo2_base::utils::ScalarField;
 use halo2_base::AssignedValue;
 use halo2_base::{
     Context,
-    QuantumCell::{Constant, Existing, Witness},
+    QuantumCell::{Constant, Existing},
 };
 use halo2_scaffold::scaffold::cmd::Cli;
 use halo2_scaffold::scaffold::run;
 use serde::{Deserialize, Serialize};
+
+use std::vec;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CircuitInput {
@@ -16,9 +18,6 @@ pub struct CircuitInput {
     pub input_string: String,
 }
 
-use std::collections::HashMap;
-
-// this algorithm takes a public input x, computes x^2 + 72, and outputs the result as public output
 fn regex_parser<F: ScalarField>(
     ctx: &mut Context<F>,
     input: CircuitInput,
@@ -34,23 +33,52 @@ fn regex_parser<F: ScalarField>(
 
     let gate = GateChip::<F>::default();
 
-    let mut dict: HashMap<F, HashMap<F, F>> = HashMap::new();
-    let mut state = F::from(0);
+    let mut transition_table: Vec<Vec<AssignedValue<F>>> = Vec::new();
+
     for c in pattern {
-        dict.insert(state, {
-            let mut entry = HashMap::new();
-            let inc = F::from(1) - *gate.is_equal(ctx, c, Constant(F::from('*' as u64))).value();
-            state = state + inc;
-            entry.insert(*c.value(), state);
-            entry
-        });
+        let is_equal = gate.is_equal(ctx, c, Constant(F::from('*' as u64)));
+        let inc = gate.sub(ctx, Constant(F::from(1)), is_equal);
+        if let Some(last_value) = transition_table.last() {
+            let last_state = *last_value.last().unwrap();
+            let next_state = gate.add(ctx, last_state, inc);
+            transition_table.push(vec![c, last_state, next_state]);
+        } else {
+            let initial_state = ctx.load_constant(F::from(1));
+            let next_state = gate.add(ctx, initial_state, inc);
+            transition_table.push(vec![c, initial_state, next_state]);
+        }
     }
 
-    let accept = ctx.load_witness(state);
-    make_public.push(accept);
+    let accept = *transition_table.last().unwrap().last().unwrap();
 
-    // println!("x: {:?}", x.value());
-    println!("val_assigned: {:?}", accept.value());
+    let next_states_vec = transition_table.iter().map(|row| Existing(row[2])).collect::<Vec<_>>();
+    let mut possible_states = vec![ctx.load_constant(F::from(1))];
+    for c in input_string {
+        let tokens = vec![Constant(F::from('*' as u64)), Constant(F::from('.' as u64)), Existing(c)];
+        let mut res = vec![];
+        for x in &possible_states {
+            for token in &tokens {
+                let one_hot_vector = transition_table.iter().map(|row| {
+                    let diff1 = gate.sub(ctx, row[0], *token);
+                    let diff2 = gate.sub(ctx, row[1], *x);
+                    let diff1_iszero = gate.is_zero(ctx, diff1);
+                    let diff2_iszero = gate.is_zero(ctx, diff2);
+                    gate.mul(ctx, diff1_iszero, diff2_iszero)
+                }).collect::<Vec<_>>();
+                res.push(gate.inner_product(ctx, one_hot_vector.clone(), next_states_vec.clone()));
+            }
+        }
+        std::mem::swap(&mut possible_states, &mut res);
+    }
+
+    let out_vec = possible_states.into_iter().map(|x| {
+        let diff = gate.sub(ctx, x, accept);
+        gate.is_zero(ctx, diff)
+    }).collect::<Vec<_>>();
+    let out = gate.sum(ctx, out_vec);
+    make_public.push(out);
+
+    println!("val_assigned: {:?}", out.value());
 }
 
 fn main() {
