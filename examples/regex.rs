28eb8e4fbe39b82122cdb46a8fb4ad56b88614fb
Stanley Zhu <syzhu00@gmail.com>
2023-06-14 19:49:38 -0700

    clean up

--- examples/regex.rs
+++ examples/regex.rs
@@ -5,7 +5,7 @@ use halo2_base::AssignedValue;
 use halo2_base::QuantumCell;
 use halo2_base::{
     Context,
-    QuantumCell::{Constant, Existing, Witness},
+    QuantumCell::{Constant, Existing},
 };
 use halo2_scaffold::scaffold::cmd::Cli;
 use halo2_scaffold::scaffold::run;
@@ -41,16 +41,13 @@ fn add_state<F: ScalarField>(ctx: &mut Context<F>,
 fn lookup_transition<F: ScalarField>(ctx: &mut Context<F>,
     gate: &GateChip<F>,
     transition_table: &Vec<Vec<AssignedValue<F>>>,
-    next_states_vec: &Vec<QuantumCell<F>>,
-    state: F,
+    state: QuantumCell<F>,
     character: QuantumCell<F>) -> AssignedValue<F> {
-    let state = Witness(state);
+    let next_states_vec = transition_table.iter().map(|row| Existing(row[1])).collect::<Vec<_>>();
+    let big_value = Constant(F::from(2_u64.pow(10)));
+    let key = gate.mul_add(ctx, character, big_value, state);
     let one_hot_vector = transition_table.iter().map(|row| {
-        let diff1 = gate.sub(ctx, row[0], character);
-        let diff2 = gate.sub(ctx, row[1], state);
-        let diff1_iszero = gate.is_zero(ctx, diff1);
-        let diff2_iszero = gate.is_zero(ctx, diff2);
-        gate.mul(ctx, diff1_iszero, diff2_iszero)
+        gate.is_equal(ctx, key, row[0])
     }).collect::<Vec<_>>();
     gate.inner_product(ctx, one_hot_vector.clone(), next_states_vec.clone())
 }
@@ -60,7 +57,6 @@ fn lookup_transition<F: ScalarField>(ctx: &mut Context<F>,
 fn epsilon_closure<F: ScalarField>(ctx: &mut Context<F>,
     gate: &GateChip<F>,
     transition_table: &Vec<Vec<AssignedValue<F>>>,
-    next_states_vec: &Vec<QuantumCell<F>>,
     states: &Vec<AssignedValue<F>>) -> Vec<AssignedValue<F>> {
     let mut cur_states = states.clone();
 
@@ -68,7 +64,7 @@ fn epsilon_closure<F: ScalarField>(ctx: &mut Context<F>,
         let mut next: Vec<AssignedValue<F>> = cur_states.clone();
         for i in 1..MAX_PATTERN_LEN {
             let state_exists = gate.is_equal(ctx, cur_states[i], Constant(F::from(1)));
-            let index = lookup_transition(ctx, gate, transition_table, next_states_vec, F::from(i as u64), Constant(F::from('*' as u64)));
+            let index = lookup_transition(ctx, gate, transition_table, Constant(F::from(i as u64)), Constant(F::from('*' as u64)));
             let to_add = gate.mul(ctx, index, state_exists);
             next = add_state(ctx, &gate, &next, &to_add);
         }
@@ -111,6 +107,8 @@ fn regex_parser<F: ScalarField>(
     let mut state = ctx.load_constant(F::from(1));
     let mut accept = state;
 
+    let big_value = ctx.load_constant(F::from(2_u64.pow(10)));
+
     for i in 0..(MAX_PATTERN_LEN - 1) {
         let c = pattern[i];
         let valid = range.is_less_than(ctx, Constant(F::from(i as u64)), pattern_len, 10);
@@ -121,18 +119,19 @@ fn regex_parser<F: ScalarField>(
         // Increment the state if it's not followed by an asterisk
         let inc = gate.sub(ctx, Constant(F::from(1)), followed_by_asterisk);
         let next_state: AssignedValue<F> = gate.add(ctx, state, inc);
-        transition_table.push(vec![c, state, next_state]);
+        // transition_table.push(vec![c, state, next_state]);
+        let key = gate.mul_add(ctx, c, big_value, state);
+        transition_table.push(vec![key, next_state]);
         // Only consider the valid part of the string
         accept = gate.select(ctx, next_state, accept, valid);
         state = next_state;
     }
 
-    let next_states_vec = transition_table.iter().map(|row| Existing(row[2])).collect::<Vec<_>>();
     let initial_state = (0..MAX_PATTERN_LEN).map(|i| {
         if i == 1 { ctx.load_constant(F::from(1)) } else { ctx.load_zero() }
     }).collect::<Vec<_>>();
 
-    let mut possible_states = epsilon_closure(ctx, &gate, &transition_table, &next_states_vec, &initial_state);
+    let mut possible_states = epsilon_closure(ctx, &gate, &transition_table, &initial_state);
 
     for i in 0..MAX_INPUT_LEN {
         let mut next_states = [(); MAX_PATTERN_LEN].map(|_| ctx.load_zero()).to_vec();
@@ -140,16 +139,16 @@ fn regex_parser<F: ScalarField>(
         let character = input_string[i];
         for j in 0..MAX_PATTERN_LEN {
             let state_exists = possible_states[j];
-            let transition1 = lookup_transition(ctx, &gate, &transition_table, &next_states_vec, F::from(j as u64), Existing(character));
+            let transition1 = lookup_transition(ctx, &gate, &transition_table, Constant(F::from(j as u64)), Existing(character));
             // states that can be reached through a dot edge
-            let transition2 = lookup_transition(ctx, &gate, &transition_table, &next_states_vec, F::from(j as u64), Constant(F::from('.' as u64)));
+            let transition2 = lookup_transition(ctx, &gate, &transition_table, Constant(F::from(j as u64)), Constant(F::from('.' as u64)));
             // only add the state if states_exists = 1
             let to_add1 = gate.mul(ctx, transition1, state_exists);
             let to_add2 = gate.mul(ctx, transition2, state_exists);
             next_states = add_state(ctx, &gate, &next_states, &to_add1);
             next_states = add_state(ctx, &gate, &next_states, &to_add2);
         }
-        next_states = epsilon_closure(ctx, &gate, &transition_table, &next_states_vec, &next_states);
+        next_states = epsilon_closure(ctx, &gate, &transition_table, &next_states);
         possible_states = (0..MAX_PATTERN_LEN).into_iter().map(|k| {
             gate.select(ctx, next_states[k], possible_states[k], valid)
         }).collect::<Vec<_>>();
